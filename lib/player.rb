8feaf7d13b8f04691d02961383bc1d73b9212281
Roberto-Livi <robertomlivi@outlook.com>
2020-12-26 14:14:13 -0500

    game.rb in progess

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
-class Player 
-  attr_reader :token
-  
-  def initialize(token)
-    @token = token
-  end
+class Player
+    attr_reader :token
+
+    def initialize(token)
+        @token = token
+    end
 end
